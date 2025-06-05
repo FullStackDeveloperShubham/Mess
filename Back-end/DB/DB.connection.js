40f392357bfb72829eee7ebc82f9d262e6aa7034
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-06-05 19:10:44 +0530

    Connected to mongodb , nodemon and mongoose install

---
+++ Back-end/DB/DB.connection.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// connect to MongoDB
+const connectDB = async ()=>{
+    try {
+         const connection = await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`MongoDB connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1); 
+    }
+}
+
+export default connectDB;
--- Back-end/server.js
+++ Back-end/server.js
@@ -1,1 +1,24 @@
-this is server file
+import express from "express";
+import connectDB from "./DB/DB.connection.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = 3000 || process.env.PORT;
+
+const app = express();
+
+// simple route
+app.get("/",(req,res)=>{
+res.send("Welcome to the Express.js server!");
+})
+
+
+// listen the server
+app.listen(port, () => {
+  // connect to MongoDB
+  connectDB()
+    .then(() => console.log(`http://localhost:${port}`))
+    .catch((error) =>
+      console.error("Error connecting to MongoDB:", error.message)
+    );
+});
